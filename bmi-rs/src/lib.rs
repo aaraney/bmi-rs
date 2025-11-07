1b67f37ae5f041a4a132fcb4dbc3dca4fdcf21d2
Austin Raney <aaraney@protonmail.com>
2025-11-07 16:37:46 -0500

    chore!: re-export members under bmi_rs namespace. remove bmi_rs::wrapper export
    export mod errors

--- bmi-rs/src/lib.rs
+++ bmi-rs/src/lib.rs
@@ -3,4 +3,10 @@
 /// _like_ trait and functionality to expose implementations over ffi via the
 /// [bmi-c interface](https://github.com/csdms/bmi-c).
 pub mod bmi;
-pub mod wrapper;
+
+pub mod errors;
+
+mod wrapper;
+pub use crate::bmi::{
+    Bmi, BmiResult, GridType, Location, RefValues, ValueType, Values, register_model,
+};
